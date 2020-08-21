4a938c53f9a0479f62216ad54339f1b512e2dda4
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-08-21 16:17:26 +0430

    adding localStorage for save data in movie seat app

--- Movie Seat App/script.js
+++ Movie Seat App/script.js
@@ -8,21 +8,64 @@ const movieSelect = document.getElementById('movie')
 
 let tiketPrice = +movieSelect.value;
 
+
+populateUi();
+
+// set selected movie and price
+
+function setMovieData(movieIndex, moviePrice) {
+
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+
+}
+
+
 ///update total and count
 
 function updateSelectedCount() {
 
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const seatsIndex = [...selectedSeats].map((seat) =>
+        [...seats].indexOf(seat)
+    )
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     const selectedSeatsCount = selectedSeats.length
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * tiketPrice
 
 }
 
+/// Get data from localStorage nad populate data
+
+
+function populateUi() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+
+
+}
+
+
 ///movie select event
 
 movieSelect.addEventListener('change', e => {
     tiketPrice = +e.target.value
+
     updateSelectedCount()
 
 
@@ -33,10 +76,15 @@ movieSelect.addEventListener('change', e => {
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected')
+        setMovieData(e.target.selectedIndex, e.target.value)
         updateSelectedCount()
 
     }
 })
 
 
+
+///initial count and total set
+
+updateSelectedCount()
 
